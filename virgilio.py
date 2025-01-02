6fbe8e98678776d5baf20086770dbb35f04b6678
MoEzwawi <mezwawi96@gmail.com>
2025-01-02 16:54:41 +0100

    Exercise 1

--- virgilio.py
+++ virgilio.py
@@ -1,3 +1,13 @@
+import os
+
 class Virgilio:
     def __init__(self, directory):
         self.directory = directory
+
+    def read_canto_lines(self, canto_number):
+        file_path = os.path.join(self.directory, f"Canto_{canto_number}.txt")
+        with open(file_path, "r", encoding="utf-8") as file:
+            return file.readlines()
+
+virgilio_instance = Virgilio("canti")
+print(virgilio_instance.read_canto_lines(5))
